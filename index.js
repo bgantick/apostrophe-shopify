79477cd3fd4afce9f7eb0b72770da7f598979c38
Brian Gantick <bgantick@gmail.com>
2020-06-21 15:45:10 -0400

    Initial module config

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moogBundle: {
+    modules: [
+      'apostrophe-shopify-products',
+      'apostrophe-shopify-products-pages',
+      'apostrophe-shopify-products-widgets',
+      'apostrophe-shopify-collections'
+    ],
+    directory: 'lib/modules'
+  }
+};
