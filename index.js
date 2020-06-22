e6404aef2a9d109c2e9aea0db249bf484dc67057
Brian Gantick <bgantick@gmail.com>
2020-06-21 20:12:41 -0400

    Admin bar config

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 module.exports = {
   moogBundle: {
     modules: [
+      'apostrophe-shopify-collections',
       'apostrophe-shopify-products',
       'apostrophe-shopify-products-pages',
-      'apostrophe-shopify-products-widgets',
-      'apostrophe-shopify-collections'
+      'apostrophe-shopify-products-widgets'
     ],
     directory: 'lib/modules'
+  },
+  construct: (self, options) => {
+    self.addToAdminBar = () => {
+      const items = [];
+      self.apos.adminBar.add(self.__meta.name + '-sync', 'Shopify Sync');
+      items.push(self.__meta.name + '-sync')
+      self.apos.adminBar.group({
+        label: 'Shopify',
+        items: items
+      });
+    }
+  },
+  afterConstruct: (self) => {
+    self.addToAdminBar();
   }
 };
